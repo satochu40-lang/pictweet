98b442bc79f1c37c2158b512907f73ea089ab9a0
satochu40-lang <satochu.40@gmail.com>
2026-08-21 22:53:14 +0900

    showアクションのリクエストスペック実装

--- spec/requests/tweets_spec.rb
+++ spec/requests/tweets_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
-describe TweetsController, type: :request do
 
+RSpec.describe 'TweetsController', type: :request do
   before do
     @tweet = FactoryBot.create(:tweet)
   end
@@ -8,13 +8,48 @@ describe TweetsController, type: :request do
   describe 'GET #index' do
     it 'indexアクションにリクエストすると正常にレスポンスが返ってくる' do 
       get root_path
-       expect(response.status).to eq 200
+      expect(response.status).to eq 200
     end
+
     it 'indexアクションにリクエストするとレスポンスに投稿済みのツイートのテキストが存在する' do 
+      get root_path
+      expect(response.body).to include(@tweet.text)
     end
+
     it 'indexアクションにリクエストするとレスポンスに投稿済みのツイートの画像URLが存在する' do 
-    end
+      get root_path
+      expect(response.body).to include(@tweet.image)
+    end 
+
     it 'indexアクションにリクエストするとレスポンスに投稿検索フォームが存在する' do 
+      get root_path
+      expect(response.body).to include('検索')
     end
   end
+
+  describe 'GET #show' do
+    before do
+      @tweet = FactoryBot.create(:tweet) # ← これがあるか確認！
+    end
+
+    it 'showアクションにリクエストすると正常にレスポンスが返ってくる' do
+      get tweet_path(@tweet)
+      expect(response.status).to eq 200
+    end
+
+    it 'showアクションにリクエストするとレスポンスに投稿済みのツイートのテキストが存在する' do 
+       get tweet_path(@tweet)
+       expect(response.body).to include(@tweet.text)
+    end
+
+    it 'showアクションにリクエストするとレスポンスに投稿済みのツイートの画像URLが存在する' do 
+        get tweet_path(@tweet)
+        expect(response.body).to include(@tweet.image)
+    end
+
+     it 'showアクションにリクエストするとレスポンスにコメント一覧表示部分が存在する' do 
+        get tweet_path(@tweet)
+        expect(response.body).to include('＜コメント一覧＞')
+     end
+  end  
 end
